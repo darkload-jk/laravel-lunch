f76aa258790ccd352c0d641e5a62e1d5d3f0cbbe
darkload-jk <80630822+darkload-jk@users.noreply.github.com>
2023-05-30 22:12:02 +0900

    Update 2023_04_16_010647_create_prices_table.php

--- database/migrations/2023_04_16_010647_create_prices_table.php
+++ database/migrations/2023_04_16_010647_create_prices_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('prices', function (Blueprint $table) {
-            $table->id();
+            $table->id('price_id');
             $table->unsignedBigInteger('price');
         });
     }
